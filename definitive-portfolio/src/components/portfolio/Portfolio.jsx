2dc40d03d89abf6c68efdefd3888f00fd1eeffb9
allystor <alvarojoao17@gmail.com>
2023-04-09 12:20:20 -0300

    feat:finishing portfolio

--- definitive-portfolio/src/components/portfolio/Portfolio.jsx
+++ definitive-portfolio/src/components/portfolio/Portfolio.jsx
@@ -40,6 +40,10 @@ export default function Portfolio() {
   return (
     <div className='portfolio' id='intro'>
       <h1>Portfolio</h1>
+      <div className='text'>
+        <h4>Atualmente estou cursando engenharia de software e faço estágio em uma empresa de logistica localiza no Rio de Janeiro e estou adquirindo bastante conhecimento em programação e desenvolvimento Web tanto front-end quanto back-end. As linguagens de programação que eu mais possuo conhecimento são Python e Javascript. Também utilizo constantemente as ferramentas básica de desenvolvimento web como o HTML5 e CSS3. E como frameworks que utilizo para deixar meus projetos mais dinâmicos são Flask, Django, React.js e Bootstrap. Além de bancos de dados relacionais como o PostgreSQL e o MySQL utilizando também a plataforma de serviço Devops sendo estes o Docker e por fim serviços de cloud como AWS e google cloud.
+        </h4>
+      </div>
       <ul>
          {list.map(item => (
           <List title={item.title} active={ selected === item.id} setSelected={setSelected} id={item.id}/>
