9a6627d5fd33e9fc48abe95dd45d113300a00a41
allystor <alvarojoao17@gmail.com>
2023-04-09 11:54:36 -0300

    feat: new components

--- definitive-portfolio/src/components/portfolio/Portfolio.jsx
+++ definitive-portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,28 +1,58 @@
+import List from '../portfolioList/List'
 import './portfolio.scss'
+import { useEffect, useState } from 'react'
+import { featuredPortfolio, frontendPortfolio, backendPortfolio } from '../../data'
 
 export default function Portfolio() {
+    const[selected, setSelected] = useState("featured")
+    const[data, setData] = useState([])
+  const list = [
+    {
+      id: 'featured',
+      title: 'Destaques',
+    },
+    {
+      id: 'frontend',
+      title: 'Front-end',
+    },
+    {
+      id: 'backend',
+      title: 'Back-end',
+    }
+  ]
+
+  useEffect(() => {
+    switch(selected) {
+      case "featured":
+        setData(featuredPortfolio)
+        break;
+      case "frontend":
+        setData(frontendPortfolio)
+        break;
+      case "backend":
+        setData(backendPortfolio)
+        break;
+      default:
+        setData(featuredPortfolio)
+    }
+  },[selected])
+
   return (
     <div className='portfolio' id='intro'>
       <h1>Portfolio</h1>
       <ul>
-        <li className='active'>Destaques</li>
-        <li>front-end</li>
-        <li>Back-end</li>
+         {list.map(item => (
+          <List title={item.title} active={ selected === item.id} setSelected={setSelected} id={item.id}/>
+        ))}
       </ul>
       <div className="container">
-        <div className="item">
-          <img src="assets/refproject.png"/>
-          <h3>Kanban project</h3>
-        </div>
-        <div className="item">
-          <img src="assets/refproject.png"/>
-          <h3>Kanban project</h3>
-        </div>
-        <div className="item">
-          <img src="assets/refproject.png"/>
-          <h3>Kanban project</h3>
+        {data.map(d => (
+          <div className="item">
+            <img src={d.img}/>
+            <h3>{d.title}</h3>
+          </div>
+        ))}
         </div>
       </div>
-    </div>
   )
 }
---
+++ definitive-portfolio/src/data.js
@@ -0,0 +1,49 @@
+export const featuredPortfolio = [
+    {
+        id: 1,
+        title: "Destaques",
+        img: "assets/refproject.png",
+    },
+    {
+        id: 2,
+        title: "Front-end",
+        img: "assets/refproject.png",
+    },
+    {
+        id: 3,
+        title: "Back-end",
+        img: "assets/refproject.png",
+    },
+]
+
+export const frontendPortfolio = [
+    {
+        id: 2,
+        title: "Front-end",
+        img: "assets/refproject.png",
+    },
+    {
+        id: 3,
+        title: "Back-end",
+        img: "assets/refproject.png",
+    },
+
+]
+
+export const backendPortfolio = [
+    {
+        id: 1,
+        title: "Destaques",
+        img: "assets/refproject.png",
+    },
+    {
+        id: 2,
+        title: "Front-end",
+        img: "assets/refproject.png",
+    },
+    {
+        id: 3,
+        title: "Back-end",
+        img: "assets/refproject.png",
+    },
+]
