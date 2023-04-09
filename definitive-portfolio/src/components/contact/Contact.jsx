f6a21d6880fa15159079eeedd4e3cb5b58a08fd9
allystor <alvarojoao17@gmail.com>
2023-04-09 18:56:41 -0300

    feat:finish component

--- definitive-portfolio/src/components/contact/Contact.jsx
+++ definitive-portfolio/src/components/contact/Contact.jsx
@@ -1,9 +1,27 @@
 import './contact.scss'
 
 export default function Contact() {
+
+  const [message, setMessage] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true)
+  }
   return (
     <div className='contact'id='contact'>
-      <h1>Testemunhas</h1>
+      <div className="left">
+        <img src="assets/contact.svg"/>
+      </div>
+      <div className="right">
+        <h2>Contato.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder='Email' />
+          <textarea placeholder='Mensagem'></textarea>
+          <button type='submit'>Enviar</button>
+          {message && <span>Obrigado, entrarei em contato!</span>}
+        </form>
+      </div>
     </div>
   )
 }
