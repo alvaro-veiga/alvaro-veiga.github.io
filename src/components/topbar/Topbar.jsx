81134a92c20851621c01a6fe199654ec6b9df115
allystor <alvarojoao17@gmail.com>
2023-04-11 22:42:04 -0300

    feat: my informations

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -10,11 +10,15 @@ export default function Topbar({menuOpen, setMenuOpen}) {
           <a href='#intro' className='logo'>ajsv.</a>
           <div className="itemContainer">
             <RxPerson className='icon'/>
-            <span>+55 21 2391-3205</span>
+            <a href="https://api.whatsapp.com/send?phone=552123913205">
+              <span>+55 21 2391-3205</span>
+            </a>
           </div>
           <div className="itemContainer">
             <IoIosMail className='icon'/>
-            <span>alvaro.veiga.dev@gmail.com</span>
+            <a href="mailto:alvarojoao17@gmail.com">
+              <span>alvaro.veiga.dev@gmail.com</span>
+            </a>
           </div>
         </div>
         <div className='right'>
