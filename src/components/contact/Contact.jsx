114e8893241c1da176fc1aa35e1fbbf464b5a8b3
allystor <alvarojoao17@gmail.com>
2023-05-11 22:51:42 -0300

    fix: contacts icons

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -10,13 +10,13 @@ export default function Contact() {
         <h2>Contato.</h2>
         <div className="iconContainer">
           <div className="iconInstagram">
-            <a href='https://www.linkedin.com/in/%C3%A1lvaro-jo%C3%A3o-da-silva-veiga/'>
+            <a href='https://www.instagram.com/alvaro.veigones/'>
             <img src="assets/instagram.png"/>
             <span>Instagram</span>
             </a>
           </div>
           <div className="iconLinkedin">
-            <a href='https://www.instagram.com/alvaro.veigones/'>
+            <a href='https://www.linkedin.com/in/%C3%A1lvaro-jo%C3%A3o-da-silva-veiga/'>
             <img src="assets/linkedin.png"/>
             <span>Linkedin</span>
             </a>
